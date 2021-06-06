33021802f8154c8a30810961b88e92b465e64533
Kevin Patel <Kevin.Patel@veritas.com>
2021-06-05 20:35:54 -0400

    Completed module

--- main.go
+++ main.go
@@ -27,8 +27,25 @@ func main() {
 	for {
 
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+		_, err := fmt.Scanln(&originValue)
+		if err != nil {
+			printError(errReadingInput)
+			break
+		}
+
+		if originUnit == "C" {
+			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
+		}
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+		_, err = fmt.Scanln(&shouldConvertAgain)
+		if err != nil {
+			printError(errReadingInput)
+			break
+		}
+		shouldConvertAgain = strings.TrimSpace(strings.ToUpper(shouldConvertAgain))
 
 		if shouldConvertAgain != "Y" {
 			fmt.Println("Good bye!")
